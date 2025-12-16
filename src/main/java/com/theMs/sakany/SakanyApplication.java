b5a12461d64417362504788b9ca45681006b170b
M0Hatem <moh.medh.tem1@gmail.com>
2025-12-16 09:53:39 +0200

    init Commit

---
+++ src/main/java/com/theMs/sakany/SakanyApplication.java
@@ -0,0 +1,13 @@
+package com.theMs.sakany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theMs/ms/sakany/SakanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theMs.ms.sakany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
