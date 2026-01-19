c8006165e8fb8d6ee75d56c87d31a34e67cb9a43
M16 <moh.medh.tem1@gmail.com>
2026-01-19 03:59:04 +0200

    feat: Implement BusinessRuleException and enhance NotFoundException with entity details

--- src/main/java/com/theMs/sakany/shared/exception/BusinessRuleException.java
+++ src/main/java/com/theMs/sakany/shared/exception/BusinessRuleException.java
@@ -1,4 +1,8 @@
 package com.theMs.sakany.shared.exception;
 
-public class BusinessRuleException {
+public class BusinessRuleException extends RuntimeException{
+    public BusinessRuleException(String message){
+        super(message);
+    }
+
 }
--- src/main/java/com/theMs/sakany/shared/exception/NotFoundException.java
+++ src/main/java/com/theMs/sakany/shared/exception/NotFoundException.java
@@ -1,4 +1,17 @@
 package com.theMs.sakany.shared.exception;
 
-public class NotFoundException {
+import lombok.Getter;
+
+@Getter
+public class NotFoundException extends RuntimeException{
+    private final String entityName;
+    private final Object id;
+
+
+    public NotFoundException(String entityName, Object id) {
+        super(entityName + " with id '" + id + "' not found"  );
+        this.entityName = entityName;
+        this.id = id;
+    }
+
 }
