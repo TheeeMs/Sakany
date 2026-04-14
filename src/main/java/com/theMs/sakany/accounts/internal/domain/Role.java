a7265f6f799016263ce0706bdbf979c997f81a4d
M16 <moh.medh.tem1@gmail.com>
2026-04-14 13:26:04 +0200

    feat: implement User aggregate with split name fields, domain events, and supporting schema updates

---
+++ src/main/java/com/theMs/sakany/accounts/internal/domain/Role.java
@@ -0,0 +1,4 @@
+package com.theMs.sakany.accounts.internal.domain;
+
+public enum Role {
+}
---
+++ src/main/java/com/theMs/sakany/accounts/internal/domain/events/UserCreated.java
@@ -0,0 +1,4 @@
+package com.theMs.sakany.accounts.internal.domain.events;
+
+public class UserCreated {
+}
